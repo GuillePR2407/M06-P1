f95e084d45e609080679b38d142db42bf99ecbb1
GuillePR2407 <guillempr2003@gmail.com>
2023-10-31 18:25:55 +0100

    aaa

---
+++ Llista.py
@@ -0,0 +1,37 @@
+class Llista:
+    def __init__(self):
+        self.registres = []
+        self.categories = []
+
+    def get_registres(self):
+        return self.registres
+
+    def create_registre(self, registre):
+
+
+    def read_registre(self, nom_article):
+
+
+    def update_registre(self, registre):
+
+
+    def delete_registre(self, nom_article):
+
+
+    def create_categoria(self, categoria):
+
+
+    def read_categoria(self, nom_categoria):
+
+
+    def update_categoria(self, categoria):
+  
+
+    def delete_categoria(self, nom_categoria):
+       
+
+    def desa_a_disc(self):
+        pass
+
+    def llegeix_de_disc(self):
+        pass
